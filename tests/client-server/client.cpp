cb7a48df337d655f161851ba5fde8ae92e2b2b66
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2016-04-12 17:20:15 -0700

    Cleaned up client server example.

--- tests/client-server/client.cpp
+++ tests/client-server/client.cpp
@@ -1,32 +1,46 @@
-//
-//  Hello World client in C++
-//  Connects REQ socket to tcp://localhost:5555
-//  Sends "Hello" to server, expects "World" back
-//
-#include <zmq.hpp>
-#include <string>
+/*
+ * Client-Server Hello World Test
+ * Author: Pranav Srinivas Kumar
+ * Date: 2016.04.12
+ */
+
 #include <iostream>
+#include <string>
+#include <thread>
+#include <zmq.hpp>
 
-int main ()
-{
-    //  Prepare our context and socket
-    zmq::context_t context (1);
-    zmq::socket_t socket (context, ZMQ_REQ);
+void client_function() {
 
-    std::cout << "Connecting to hello world server…" << std::endl;
-    socket.connect ("tcp://localhost:5555");
+  zmq::context_t context(1);
+  zmq::socket_t socket (context, ZMQ_REQ);
+  std::cout << "Connecting to the Hello World Server" << std::endl;
 
-    //  Do 10 requests, waiting each time for a response
-    for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-        zmq::message_t request (5);
-        memcpy (request.data (), "Hello", 5);
-        std::cout << "Sending Hello " << request_nbr << "…" << std::endl;
-        socket.send (request);
+  socket.connect("tcp://localhost:5555");
+  while (true) {
 
-        //  Get the reply.
-        zmq::message_t reply;
-        socket.recv (&reply);
-        std::cout << "Received World " << request_nbr << std::endl;
-    }
-    return 0;
+    std::string message;
+    std::cout << "Enter message: ";
+    std::cin >> message;
+
+    // Prepare request message
+    zmq::message_t request (message.length());
+    memcpy(request.data(), message.c_str(), message.length());
+    std::cout << "Sending " << message << std::endl;
+    socket.send(request);
+
+    // Receive response from server
+    zmq::message_t reply;
+    socket.recv(&reply);
+    std::string reply_string = std::string(static_cast<char *>(reply.data()), 
+					   reply.size());
+    std::cout << "Received " << reply_string << std::endl;
+  }
 }
+
+
+int main() {
+  std::thread client_thread(client_function);
+  client_thread.join();
+  return 0;
+}
+
--- tests/client-server/server.cpp
+++ tests/client-server/server.cpp
@@ -1,39 +1,41 @@
-//
-//  Hello World server in C++
-//  Binds REP socket to tcp://*:5555
-//  Expects "Hello" from client, replies with "World"
-//
-#include <zmq.hpp>
-#include <string>
+/*
+ * Client-Server Hello World Test
+ * Author: Pranav Srinivas Kumar
+ * Date: 2016.04.12
+ */
+
 #include <iostream>
-#ifndef _WIN32
+#include <string>
+#include <thread>
 #include <unistd.h>
-#else
-#include <windows.h>
+#include <zmq.hpp>
 
-#define sleep(n)    Sleep(n)
-#endif
+void server_function() {
+  zmq::context_t context (1);
+  zmq::socket_t socket (context, ZMQ_REP);
+  socket.bind ("tcp://*:5555");
 
-int main () {
-    //  Prepare our context and socket
-    zmq::context_t context (1);
-    zmq::socket_t socket (context, ZMQ_REP);
-    socket.bind ("tcp://*:5555");
+  while (true) {
+    zmq::message_t request;
 
-    while (true) {
-        zmq::message_t request;
+    // Wait for a client request
+    socket.recv (&request);
+    std::string request_string = std::string(static_cast<char *>(request.data()), 
+					     request.size());
+    std::cout << "Received " << request_string << std::endl;
 
-        //  Wait for next request from client
-        socket.recv (&request);
-        std::cout << "Received Hello" << std::endl;
-
-        //  Do some 'work'
-        sleep(1);
-
-        //  Send reply back to client
-        zmq::message_t reply (5);
-        memcpy (reply.data (), "World", 5);
-        socket.send (reply);
-    }
-    return 0;
+    // Prepare and send response back
+    std::string response("ACK");
+    zmq::message_t reply (response.length());
+    memcpy (reply.data (), response.c_str(), response.length());
+    socket.send (reply);
+  }
 }
+
+int main() {
+  std::thread server_thread(server_function);
+  server_thread.join();
+  return 0;
+}
+
+
