8b3328e2cb4201a2cd473d02f9487db0357c9768
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2016-04-16 16:56:47 -0700

    Updated queue test

--- tests/queue-processing-test/queue_test.cpp
+++ tests/queue-processing-test/queue_test.cpp
@@ -2,9 +2,16 @@
 #include <string>
 #include "operation_queue.hpp"
 #include "timer.hpp"
+#include "publisher.hpp"
+#include "subscriber.hpp"
 
 void timer_function() {
-  std::cout << "Timer Operation Function" << std::endl;
+  Publisher timer_pub("timer_pub", "tcp://*:5559");
+  timer_pub.publish("timer_message 12345");
+}
+
+void subscriber_function(std::string received_message) {
+  std::cout << "Subscriber Operation : Received : " << received_message << std::endl;  
 }
 
 int main() {
@@ -12,9 +19,21 @@ int main() {
   operation_queue = new Operation_Queue();
   std::thread executor_thread = operation_queue->spawn();
 
-  Timer new_timer("timer_operation", 50, 1000000000, timer_function);
-  std::thread new_timer_thread = new_timer.spawn();
+  Subscriber new_subscriber("subscriber_operation", 
+			    60, 
+			    "timer_message", 
+			    "tcp://localhost:5559", 
+			    std::bind(&subscriber_function, 
+				      std::placeholders::_1));
+  std::thread new_subscriber_thread = new_subscriber.spawn();
 
+  Timer new_timer("timer_operation", 
+		  50, 
+		  1000000000, 
+		  timer_function);
+  std::thread new_timer_thread = new_timer.spawn();
+  
+  new_subscriber_thread.join();
   new_timer_thread.join();
   executor_thread.join();
 
