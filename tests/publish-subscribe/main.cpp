90fc100f03adfe450bcb91f1644422e3f1f8ab46
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2016-04-17 15:39:51 -0700

    Removed obsolete tests

--- tests/publish-subscribe/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include "pub_sub_component.hpp"
-
-int main() {
-  Pub_Sub_Component new_instance;
-  new_instance.run();
-  return 0;
-}
--- tests/timers/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include "timer_component.hpp"
-
-int main() {
-  Timer_Component new_instance;
-  new_instance.run();
-  return 0;
-}
